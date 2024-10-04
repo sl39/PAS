a3f6fae17e2ecf30f1485924cd6b1057c34b87cc
Gom534 <wjddntyvld@naver.com>
2024-10-04 11:51:32 +0900

    first commit

---
+++ back/src/main/java/com/ex/artion/artion/ArtionApplication.java
@@ -0,0 +1,13 @@
+package com.ex.artion.artion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtionApplication.class, args);
+	}
+
+}
---
+++ back/src/test/java/com/ex/artion/artion/ArtionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ex.artion.artion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
