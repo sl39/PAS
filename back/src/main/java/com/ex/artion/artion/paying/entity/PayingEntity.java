fd41976e5062d1f8e82c455c07c658477a934984
SewoolLee <swcl309@naver.com>
2024-10-14 18:41:52 +0900

    feat: scheduler config

--- back/src/main/java/com/ex/artion/artion/paying/entity/PayingEntity.java
+++ back/src/main/java/com/ex/artion/artion/paying/entity/PayingEntity.java
@@ -2,8 +2,9 @@ package com.ex.artion.artion.paying.entity;
 
 import com.ex.artion.artion.auction.entity.AuctionEntity;
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -11,12 +12,17 @@ import java.time.LocalDateTime;
 @Entity
 @Setter
 @Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
 public class PayingEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int paying_pk;
 
+    @CreatedDate
     private LocalDateTime createdAt;
 
     @OneToOne
