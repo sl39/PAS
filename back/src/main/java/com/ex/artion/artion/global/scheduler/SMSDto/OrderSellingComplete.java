acb07ed88fcf39778d00a84c3fd1d69b51ddbf22
SewoolLee <swcl309@naver.com>
2024-10-21 14:17:53 +0900

    feat: smsservice

---
+++ back/src/main/java/com/ex/artion/artion/global/scheduler/SMSDto/OrderSellingComplete.java
@@ -0,0 +1,17 @@
+package com.ex.artion.artion.global.scheduler.SMSDto;
+
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+public class OrderSellingComplete {
+    private String art_name;
+    private Long art_price;
+    private String order_num;
+    private String order_date;
+    private String phone_number;
+}
