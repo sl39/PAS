2b3c5b8832a7dc8fbdf16c7c8cdc1ac987069c41
SewoolLee <swcl309@naver.com>
2024-10-24 10:33:57 +0900

    feat

--- back/src/main/java/com/ex/artion/artion/global/scheduler/SchedulerConfig.java
+++ back/src/main/java/com/ex/artion/artion/global/scheduler/SchedulerConfig.java
@@ -1,19 +1,19 @@
-//package com.ex.artion.artion.global.scheduler;
-//
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
-//
-//@Configuration
-//public class SchedulerConfig {
-//    private static final int POOL_SIZE = 1;
-//
-//    @Bean
-//    public ThreadPoolTaskScheduler threadPoolTaskScheduler() {
-//        ThreadPoolTaskScheduler threadPoolTaskScheduler = new ThreadPoolTaskScheduler();
-//        threadPoolTaskScheduler.setPoolSize(POOL_SIZE);
-//        threadPoolTaskScheduler.initialize();
-//        return threadPoolTaskScheduler;
-//    }
-//}
+package com.ex.artion.artion.global.scheduler;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+
+@Configuration
+public class SchedulerConfig {
+    private static final int POOL_SIZE = 1;
+
+    @Bean
+    public ThreadPoolTaskScheduler threadPoolTaskScheduler() {
+        ThreadPoolTaskScheduler threadPoolTaskScheduler = new ThreadPoolTaskScheduler();
+        threadPoolTaskScheduler.setPoolSize(POOL_SIZE);
+        threadPoolTaskScheduler.initialize();
+        return threadPoolTaskScheduler;
+    }
+}
