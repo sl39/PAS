c9cce97a3f2a84fc14ddb385096fe6672dbb7087
SewoolLee <swcl309@naver.com>
2024-10-24 10:34:36 +0900

    feat

--- back/src/main/java/com/ex/artion/artion/global/scheduler/redisscheduler/RedisSchedulerService.java
+++ back/src/main/java/com/ex/artion/artion/global/scheduler/redisscheduler/RedisSchedulerService.java
@@ -1,13 +1,13 @@
-package com.ex.artion.artion.global.scheduler.redisscheduler;
-
-
-import lombok.RequiredArgsConstructor;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.stereotype.Service;
-
-@Service
-@RequiredArgsConstructor
-public class RedisSchedulerService {
-    private final RedisTemplate<String, Object> redisTemplate;
-
-}
+//package com.ex.artion.artion.global.scheduler.redisscheduler;
+//
+//
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.data.redis.core.RedisTemplate;
+//import org.springframework.stereotype.Service;
+//
+//@Service
+//@RequiredArgsConstructor
+//public class RedisSchedulerService {
+//    private final RedisTemplate<String, Object> redisTemplate;
+//
+//}
