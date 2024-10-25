12f839a198248d3570a7fc8fc1e2925b46289df1
SewoolLee <swcl309@naver.com>
2024-10-25 11:44:00 +0900

    feat

--- back/src/main/java/com/ex/artion/artion/order/controller/OrderController.java
+++ back/src/main/java/com/ex/artion/artion/order/controller/OrderController.java
@@ -25,7 +25,7 @@ public class OrderController {
     public ResponseEntity<OrderGetResponseDto> getOrder(@RequestParam Integer payingPk) {
         return ResponseEntity.ok(orderService.getOrder(payingPk));
     }
-    @GetMapping("/a")
+    @GetMapping("/aa")
     public String test(){
         return "test";
     }
