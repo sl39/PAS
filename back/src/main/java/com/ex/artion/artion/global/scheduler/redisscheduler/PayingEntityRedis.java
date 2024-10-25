1450bdf60eaaee49bc0308a2f72238771ef37c74
SewoolLee <swcl309@naver.com>
2024-10-25 17:21:00 +0900

    feat: redis

--- back/src/main/java/com/ex/artion/artion/global/scheduler/redisscheduler/PayingEntityRedis.java
+++ back/src/main/java/com/ex/artion/artion/global/scheduler/redisscheduler/PayingEntityRedis.java
@@ -1,25 +1,25 @@
-package com.ex.artion.artion.global.scheduler.redisscheduler;
-
-import lombok.*;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.redis.core.RedisHash;
-
-@RedisHash("paying")
-@Getter
-@Setter
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
-@Builder
-public class PayingEntityRedis {
-    @Id
-    private Integer payingPk;
-
-    private Integer status;
-    private Integer artPk;
-
-    public PayingEntityRedis(Integer paying_pk, Integer status, Integer art_pk) {
-        this.payingPk = paying_pk;
-        this.status = status;
-        this.artPk = art_pk;
-    }
-
-}
+//package com.ex.artion.artion.global.scheduler.redisscheduler;
+//
+//import lombok.*;
+//import org.springframework.data.annotation.Id;
+//import org.springframework.data.redis.core.RedisHash;
+//
+//@RedisHash("paying")
+//@Getter
+//@Setter
+//@NoArgsConstructor(access = AccessLevel.PROTECTED)
+//@Builder
+//public class PayingEntityRedis {
+//    @Id
+//    private Integer payingPk;
+//
+//    private Integer status;
+//    private Integer artPk;
+//
+//    public PayingEntityRedis(Integer paying_pk, Integer status, Integer art_pk) {
+//        this.payingPk = paying_pk;
+//        this.status = status;
+//        this.artPk = art_pk;
+//    }
+//
+//}
--- back/src/main/java/com/ex/artion/artion/global/scheduler/redisscheduler/PayingRedisRepository.java
+++ back/src/main/java/com/ex/artion/artion/global/scheduler/redisscheduler/PayingRedisRepository.java
@@ -1,9 +1,9 @@
-package com.ex.artion.artion.global.scheduler.redisscheduler;
-
-import org.springframework.data.repository.CrudRepository;
-
-import java.util.Optional;
-
-public interface PayingRedisRepository extends CrudRepository<PayingEntityRedis, Integer> {
-    Optional<PayingEntityRedis> findByArtPk(Integer artId);
-}
+//package com.ex.artion.artion.global.scheduler.redisscheduler;
+//
+//import org.springframework.data.repository.CrudRepository;
+//
+//import java.util.Optional;
+//
+//public interface PayingRedisRepository extends CrudRepository<PayingEntityRedis, Integer> {
+//    Optional<PayingEntityRedis> findByArtPk(Integer artId);
+//}
